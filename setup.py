4913d3270230df72700bfdc0e80878afd1bfa6af
Alex Clark <aclark@aclark.net>
2014-02-01 18:14:51 -0500

    Add content

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
+    entry_points={
+        'z3c.autoinclude.plugin': 'target = plone',
+    }
     name='plone_archetype',
 )
