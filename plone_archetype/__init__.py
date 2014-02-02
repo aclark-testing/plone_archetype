a2ebf9e5f9e4789bf9bfa629e95ce44f9470a70a
Alex Clark <aclark@aclark.net>
2014-02-02 17:33:33 -0500

    Add content

--- plone_archetype/__init__.py
+++ plone_archetype/__init__.py
@@ -1,6 +1,9 @@
 from Products.Archetypes import atapi
 from Products.ATContentTypes.content import base
 from Products.ATContentTypes.content import schemata
+from zope.interface import Interface
+from zope.interface import implements
+from zope import schema
 
 example_type_schema = atapi.Schema()
 
@@ -8,9 +11,16 @@ ExampleTypeSchema = schemata.ATContentTypeSchema.copy()
 ExampleTypeSchema += example_type_schema
 
 
+class IExampleType(Interface):
+    """ 
+    """ 
+    newfield = schema.TextLine()
+
+
 class ExampleType(base.ATCTContent):
     """
     """
+    implements(IExampleType)
 
 
 schemata.finalizeATCTSchema(ExampleTypeSchema)
